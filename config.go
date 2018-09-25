78529cf03f21aefab5476662abe6c235ac5737f2
Ben Best <ben.best@olmgroup.com>
2018-09-25 20:51:59 +0100

    Some tidying up

--- config.go
+++ config.go
@@ -1,8 +1,17 @@
 package timelogutil
 
+import "time"
+
+// Configuration is used for configuration properties
 type Configuration struct {
 	MongoDBHost  string
 	Database     string
 	AuthUserName string
 	AuthPassword string
+}
+
+// Task describes what it is that is being worked on at any time
+type Task struct {
+	Time  time.Time
+	Event string
 }
